bb3ffd1c801f6bc87d8e37ee80b01c9d0378c530
nadarjustin <69745601+nadarjustin@users.noreply.github.com>
2020-10-15 15:32:29 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,6 @@
+#add this file in the same directory where urls.py is located
+from django.http import HttpResponse
+
+def home(request):
+	# add your individual unique string into the response
+    return HttpResponse("Hello, world. a979ca24 is the polls index.") 
